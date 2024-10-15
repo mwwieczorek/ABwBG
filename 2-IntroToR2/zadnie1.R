1de67df917d3a4e1ae358559be60d9d80b0c5ff0
mwwieczorek <martyna.wieczorek18@wp.pl>
2024-10-15 10:58:51 +0200

    Add files via upload

---
+++ 2-IntroToR2/zadnie1.R
@@ -0,0 +1,21 @@
+
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+
+#-------------------------------funkcje paneli ---------------------------
+    
+#Source (Edytor skryptów) służy do pisania skryptów, a funkcje nie są automatycznie wykonywane
+#Console (Konsola) służy do wyświetlania juz uruchomionych funkcji
+#Environment wyswietla wszystkie przypisane wartosci 
+#Output panel - otwieranie plików i wyświetlanie plików z komputera.
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+#-----------------------------motyw kolorystyczny ----------------------------
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#Lucida console - najbardziej przyjazny dla oka wg mnie :)
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
